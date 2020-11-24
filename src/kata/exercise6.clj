6194438bd5820a4852c333b2763cff3dc256bec7
Iger <iger@iger.lv>
2020-11-25 00:17:59 +0200

    - ex6

---
+++ src/kata/exercise6.clj
@@ -0,0 +1,13 @@
+(ns kata.exercise6
+  (:use [clojure.test]
+        [kata.data]
+        [kata.matchers]))
+
+(testing "Numbers Seq"
+
+  ; Create a seq only with multiples of 3, starting from 0, size of 10, by using for example (range ...)
+  ;(let [numbers-list []]
+  (let [numbers-list (range 0 30 3)]
+
+    (is (= numbers-list
+           [0, 3, 6, 9, 12, 15, 18, 21, 24, 27]))))
