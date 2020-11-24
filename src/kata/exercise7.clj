ea290a1dcc861aa039aef0f529ff98f5457055aa
Iger <iger@iger.lv>
2020-11-25 00:38:32 +0200

    - ex7

---
+++ src/kata/exercise7.clj
@@ -0,0 +1,21 @@
+(ns kata.exercise7
+  (:use [clojure.test]
+        [kata.data]
+        [kata.matchers]))
+
+(testing "Average Age"
+
+  ; Calculate the average of customer ages by using your own (avg ...)
+  ;(let [avg-age -1]
+  (let [avg-age ((fn [numbers] (/ (apply + numbers) (count numbers))) (map :age (:customers mall)))]
+
+    (is (= avg-age (rationalize 28.7)))))
+
+
+(testing "How Much for Everything?!"
+
+  ; Calculate the sum of the prices for all the items in all shops by using (flatten ...)
+  ;(let [total -1]
+  (let [total (apply + (map :price (flatten (map :items (:shops mall)))))]
+
+    (is (= total 60930))))
