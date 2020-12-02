26bc8445437912ae156f0b10831a550fa1030691
Iger <iger@iger.lv>
2020-12-02 18:33:19 +0200

    - Solve Ariana Grande

--- src/kata/exercise8.clj
+++ src/kata/exercise8.clj
@@ -9,11 +9,29 @@
 
     (is (= not-for-sale #{"bag", "pants", "coat"}))))
 
+(defn wanted-total [wishlist price-list]
+  (reduce + (map #(price-list (:name %) 0) wishlist)))
+
+(defn rich-enough [price-list customer]
+  (>= (:budget customer) (wanted-total (:wants-to-buy customer) price-list)))
+
+(defn cheapest-items-only [shops]
+  (map first
+       (partition-by :name
+                     (sort-by (juxt :name :price)
+                              (flatten (map :items shops))))))
+
+(defn build-price-list [shops]
+  (into {}
+        (map (fn [item] {(:name item) (:price item)})
+             (cheapest-items-only shops))))
+
 (testing "I see it, I like it, I want it, I got it "
 
   ; Create a customers' name list including who are having enough money to buy all items they want which is on sale.
   ; Items that are not for sale can be counted as 0 money cost.
   ; If there are multiple items with the same names, but different prices, customer will choose the cheapest one.
-  (let [richies []]
+  (let [richies (map :name
+                     (filter #(rich-enough (build-price-list (:shops mall)) %) (:customers mall)))]
 
     (is (= richies ["Joe", "Patrick", "Chris", "Kathy", "Alice", "Andrew", "Amy"]))))
